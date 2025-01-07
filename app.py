9a0d876ea72b96c478ffe40c236bd4bda1cee04d
fengadp <88819531+fengadp@users.noreply.github.com>
2025-01-07 15:41:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -82,7 +82,7 @@ cols_2[1].image('./asset/QR_Web.svg', width=150, caption="https://fenett-2025.st
 tabs[0].write('')
 tabs[0].subheader('ผู้สนับสนุนการประชุมวิชาการ')
 cols_3 = tabs[0].columns(2)
-cols_3[0].image('./asset/S1.jpg')
+cols_3[0].image('./asset/S1.png')
 cols_3[1].image('./asset/S2.jpg')
 
 #tabs[0].divider()
