39eb39212f6c6e1f4554468bf23bb386ade70cc2
fengadp <88819531+fengadp@users.noreply.github.com>
2025-01-13 18:19:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -87,8 +87,8 @@ cols_3[1].image('./asset/S2.jpg')
 cols_3[2].image('./asset/S3.jpg')
 
 cols_4 = tabs[0].columns(3)
-cols_4[0].image('./asset/S4.jpg')
-cols_4[1].image('./asset/S5.png')
+cols_4[0].image('./asset/S5.png')
+cols_4[1].image('./asset/S4.jpg')
 
 #tabs[0].divider()
 
